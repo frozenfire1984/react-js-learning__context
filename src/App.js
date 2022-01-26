9a50979a9818dba02e27eb6d76be07307fe8af69
ykononov1984 <ykononov@hill30.com>
2022-01-26 21:35:16 +0300

    Updated; context with theme added; watcher of the system color theme added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
+import { themes, ThemeContext } from './contexts/theme-context'
+import PageWrapper from './components/PageWrapper'
+import './styles/style.css'
 
 function App() {
+  const [darkThemeMQ] = useState(window.matchMedia("(prefers-color-scheme: dark)"))
+  const [currentTheme, setCurrentTheme] = useState(themes.light);
+  
+  //console.log(darkThemeMQ)
+  
+  const toggleTheme = () => {
+    setCurrentTheme((prevCurrentTheme) =>
+      prevCurrentTheme === themes.light ? themes.dark : themes.light
+    )
+  }
+  
+  useEffect(() => {
+    if (darkThemeMQ.matches) {
+      //console.log("dark")
+      setCurrentTheme(themes.dark)
+    } else {
+      //console.log("light")
+      setCurrentTheme(themes.light)
+    }
+    
+    console.log("darkThemeMQ.matches:")
+    console.log(darkThemeMQ.matches)
+  },[darkThemeMQ])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="global">
+      <ThemeContext.Provider value={{theme: currentTheme, toggleTheme}}>
+        <div className="app">
+          <button onClick={() => toggleTheme()}>Toggle</button>
+          <button onClick={() => setCurrentTheme(themes.dark)}>dark</button>
+          <button onClick={() => setCurrentTheme(themes.light)}>light</button>
+          <PageWrapper/>
+        </div>
+      </ThemeContext.Provider>
     </div>
   );
 }
